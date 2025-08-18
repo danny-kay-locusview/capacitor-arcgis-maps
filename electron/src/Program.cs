40edd4f926b4d3f3a4dbe1d78f3fc032620d3b4d
Danny Kay <danny.kay@locusview.com>
2025-08-18 13:34:26 +0300

    Windows setup continued

---
+++ electron/src/Program.cs
@@ -0,0 +1,23 @@
+﻿using ElectronCgi.DotNet;
+
+namespace ArcGisMaps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new ConnectionBuilder().WithLogging().Build();
+
+            connection.OnAsync("echo", async () =>
+            {
+                var call = new TaskCompletionSource<object>();
+
+                call.SetResult("Hello World!");
+
+                return call.Task;
+            });
+
+            connection.Listen();
+        }
+    }
+}
